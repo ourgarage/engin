793186300e9cadaa70958349d3083bde9e39aa56
Dinar Garipov <garipov.dinar@gmail.com>
2016-04-18 23:40:15 +0200

    ENG-12 added yield to site index

--- resources/views/site/main.blade.php
+++ resources/views/site/main.blade.php
@@ -6,10 +6,6 @@
     <link rel="stylesheet" href="/packages/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="container">
-        <div class="text-center jumbotron">
-            <h2>This is the index page of the site. It's face :)</h2>
-        </div>
-    </div>
+    @yield('body')
 </body>
 </html>
