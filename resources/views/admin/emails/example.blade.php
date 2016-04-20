cc4febab8c17c58841fec825f7f4dd354d1ac857
Anonim_Khb <alvo4444@gmail.com>
2016-04-20 14:50:43 +1000

    ENG-7 add template and empty example

---
+++ resources/views/admin/emails/example.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.emails.main')
+
+@section('css')
+
+    /* CSS */
+
+@endsection
+
+@section('body')
+
+    /* BODY */
+
+@endsection
---
+++ resources/views/admin/emails/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="ru">
+
+<head>
+
+    <meta charset="utf-8">
+
+    @yield('css')
+
+</head>
+
+<body>
+
+@yield('body')
+
+</body>
+
+</html>
