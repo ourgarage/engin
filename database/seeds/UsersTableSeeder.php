e601b49765205e7dfdbeabf9ce4e3d110f2949f9
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-04-21 22:22:15 +1100

    Added one user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,20 +1,16 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder
 {
 
     public function run()
     {
-        $faker = Faker::create();
-        foreach (range(1,7) as $index) {
-            DB::table('users')->insert([
-                'name' => $faker->name,
-                'email' => $faker->email,
-                'password' => bcrypt('secret'),
-            ]);
-        }
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'example@gmail.com',
+            'password' => bcrypt('123'),
+        ]);
     }
 }
