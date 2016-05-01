f22257c99d5fcbe7051be5b8edf0ed02f3d8bb1e
Anonim-Khb <alvo4444@gmail.com>
2016-05-02 08:55:46 +1000

    ENG-20 Fixed notifications page group

--- resources/views/basis/notifications-page.blade.php
+++ resources/views/basis/notifications-page.blade.php
@@ -1,8 +1,3 @@
 @If(Notifications::byGroup('page')->has())
-
-<ul>
-    @foreach(Notifications::byGroup('page')->get() as $error)
-        <li>{!! $error['message'] !!}</li>
-    @endforeach
-</ul>
+    {!! Notifications::byGroup('page')->toHTML() !!}
 @endif
