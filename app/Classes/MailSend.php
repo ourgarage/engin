7c63fe97da904e993b1f7ffc84ac1cce2b958d2b
Anonim-Khb <alvo4444@gmail.com>
2016-05-03 23:14:16 +1000

    ENG-20 In Process

--- app/Classes/MailSend.php
+++ app/Classes/MailSend.php
@@ -7,15 +7,6 @@ use Mail;
 class MailSend
 {
 
-    public function register($user, $token)
-    {
-        Mail::queue(['emails.admin.register-html', 'emails.admin.register-text'],
-            ['user' => $user, 'token' => $token], function ($m) use ($user) {
-
-            $m->to($user->email, $user->name)->subject('Register');
-        });
-    }
-
     public function passwordReset($email, $token)
     {
         Mail::queue(['emails.admin.password-reset-html', 'emails.admin.password-reset-text'],
