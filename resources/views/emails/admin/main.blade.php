cc16156b3d5ff4fb1b02d9ec39e230ea78759ffb
Anonim-Khb <alvo4444@gmail.com>
2016-05-31 07:03:14 +1000

    ENG-27 Done

--- resources/views/emails/admin/main.blade.php
+++ resources/views/emails/admin/main.blade.php
@@ -6,7 +6,7 @@
 <body style="padding: 1em; background: #e9eaed;">
 <div style="background: #ffffff; padding: 1em;">
     <div style="background: #3870a9; border-radius: .26em .26em 0 0; padding: 2.3em 0;">
-        <img src="{{ asset(config('site.logo_mini')) }}" alt="logo"
+        <img src="{{ asset(config('site.logo_full_white_1')) }}" alt="logo"
              style="max-width: 75vw; height: 4em; display: block; margin: 0 auto;">
         <p style="text-align: center; margin: 1em 0 0 0; color: #f4f4f4; text-transform: uppercase">
             {{ config('site.name_full') }}
