19de813463e4bfb2105f46e3ad309ea0ee68042d
Egor Zhelobanov <egor@zhelobanov.ru>
2016-06-07 21:40:22 +1100

    Fixed controller

--- app/Http/Controllers/FilesUploadController.php
+++ app/Http/Controllers/FilesUploadController.php
@@ -12,28 +12,7 @@ use App\DTO\ImageResizeDTO;
 class FilesUploadController extends Controller
 {
 
-    public function upload(ImageResizeDTO $dto, Request $request)
-    {
-        $dto = new ImageResizeDTO();
-
-        $dto->setHeight(1300);
-        $dto->setWidth(1300);
-        $dto->setFolder('/upload/');
-        $dto->setCallback($request->CKEditorFuncNum);
-        $dto->setPath(public_path($dto->getFolder()));
-        $dto->setImage($request->file('upload'));
-        $dto->setFilename(md5(date("YmdHis") . rand(5, 50)) . '.' . $dto->getImage()->getClientOriginalExtension());
-
-        $newImage = Image::make($dto->getImage())->resize($dto->getWidth(), $dto->getHeight(), function ($constraint) {
-            $constraint->aspectRatio();
-        });
-
-        $newImage->save($dto->getPath() . $dto->getFilename(), 95);
-
-        return "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(" . $dto->getCallback() . ",  \"" . $dto->getFolder() . $dto->getFilename() . "\");</script>";
-    }
-
-    /*public function upload(Request $request)
+    public function upload(Request $request)
     {
         $dto = new ImageResizeDTO();
 
@@ -62,6 +41,6 @@ class FilesUploadController extends Controller
         $newImage->save($dto->getPath() . $dto->getFilename(), 95);
 
         return "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(" . $dto->getCallback() . ",  \"" . $dto->getFolder() . $dto->getFilename() . "\");</script>";
-    }*/
+    }
 
 }
