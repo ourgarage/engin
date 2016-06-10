bc8e7b9436b002cd4bb0178db96aa232434f0c88
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-06-10 14:52:47 +1100

    Completed...

--- app/Http/Controllers/FilesUploadController.php
+++ app/Http/Controllers/FilesUploadController.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests;
-use Validator;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Response;
+use App\Http\Requests\FilesUploadRequest;
 use Intervention\Image\ImageManagerStatic as Image;
 use App\DTO\ImageResizeDTO;
-use App\Http\Requests\FilesUploadRequest;
 
 class FilesUploadController extends Controller
 {
@@ -25,18 +20,7 @@ class FilesUploadController extends Controller
         $dto->setPath(public_path($dto->getFolder()));
         $dto->setImage($request->file('upload'));
         $dto->setFilename(md5(date("YmdHis") . rand(5, 50)) . '.' . $dto->getImage()->getClientOriginalExtension());
-/*
-        $validator = Validator::make($request->all(), [
-            'upload' => 'image|max:10',
-        ]);
 
-        if ($validator->fails()) {
-            $messages = $validator->errors()->first();
-            return "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(" . $dto->getCallback() . ",  \"" . $dto->getFolder() . $dto->getFilename() . "\", \"" . $messages . "\");</script>";
-        } else {
-            return $this->imageResize($dto);
-        }
-*/
         return $this->imageResize($dto);
     }
 
@@ -52,14 +36,9 @@ class FilesUploadController extends Controller
 
     public function saveImage(ImageResizeDTO $dto, $newImage)
     {
-        if (!$newImage->save($dto->getPath() . $dto->getFilename(), 95)) {
-            $message = trans('uploads.error');
-        } else {
-            $message = '';
-        }
+        $newImage->save($dto->getPath() . $dto->getFilename(), 95);
 
-        return "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(" . $dto->getCallback() . ",  \"" . $dto->getFolder() . $dto->getFilename() . "\", \"" . $message . "\");</script>";
-
+        return '<script type="text/javascript">window.parent.CKEDITOR.tools.callFunction("' . $dto->getCallback() . '", "' . $dto->getFolder() . $dto->getFilename() . '")</script>';
     }
 
 }
--- app/Http/Requests/FilesUploadRequest.php
+++ app/Http/Requests/FilesUploadRequest.php
@@ -3,25 +3,16 @@
 namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
-use Response;
+use Illuminate\Contracts\Validation\Validator;
 
 class FilesUploadRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
+
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         $rules = [
@@ -31,11 +22,12 @@ class FilesUploadRequest extends Request
         return $rules;
     }
 
-    public function response(Array $errors, Request $request)
+    public function formatErrors(Validator $validator)
     {
-        //return Response::json($errors->first());
-        //$file = '/upload/' . md5(date("YmdHis") . rand(5, 50)) . '.' . $request->file('upload')->getClientOriginalExtension();
-        //return "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(" . $request->CKEditorFuncNum . ",  \"" . $file . "\", \"" . $errors->first() . "\");</script>";
-    dd($request->all());
+        foreach ($validator->errors()->all() as $error) {
+            echo '<script type="text/javascript">window.parent.CKEDITOR.tools.callFunction("'.$this->CKEditorFuncNum.'", "", "'.$error.'")</script>';
+        }
+
+        return $validator->errors()->getMessages();
     }
 }
