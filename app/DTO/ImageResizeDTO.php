60bbc07979391acce2cdff01d4af5082acc4461f
Anonim-Khb <alvo4444@gmail.com>
2016-06-13 22:40:43 +1000

    ENG-41 Fix

--- app/DTO/ImageResizeDTO.php
+++ app/DTO/ImageResizeDTO.php
@@ -4,74 +4,140 @@ namespace App\DTO;
 
 class ImageResizeDTO
 {
+    /**
+     * @var
+     */
     private $width;
+    /**
+     * @var
+     */
     private $height;
+    /**
+     * @var
+     */
     private $path;
+    /**
+     * @var
+     */
     private $image;
+    /**
+     * @var
+     */
     private $filename;
+    /**
+     * @var
+     */
     private $quality;
 
+    /**
+     * @param $width
+     * @return $this
+     */
     public function setWidth($width)
     {
         $this->width = $width;
+
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
     public function getWidth()
     {
         return $this->width;
     }
 
+    /**
+     * @param $height
+     * @return $this
+     */
     public function setHeight($height)
     {
         $this->height = $height;
+
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
     public function getHeight()
     {
         return $this->height;
     }
 
+    /**
+     * @param $path
+     * @return $this
+     */
     public function setPath($path)
     {
         $this->path = $path;
+
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
     public function getPath()
     {
         return $this->path;
     }
 
+    /**
+     * @param $image
+     * @return $this
+     */
     public function setImage($image)
     {
         $this->image = $image;
+
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
     public function getImage()
     {
         return $this->image;
     }
 
+    /**
+     * @param $filename
+     * @return $this
+     */
     public function setFilename($filename)
     {
         $this->filename = $filename;
+
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
     public function getFilename()
     {
         return $this->filename;
     }
 
+    /**
+     * @param $quality
+     * @return $this
+     */
     public function setQuality($quality)
     {
         $this->quality = $quality;
+
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
     public function getQuality()
     {
         return $this->quality;
