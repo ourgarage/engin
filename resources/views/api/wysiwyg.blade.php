2ed3ffe5a4022ee1800b34b057fe19dcdaa88c57
Anonim-Khb <alvo4444@gmail.com>
2016-06-14 22:26:40 +1000

    ENG-41 fantasy mix

---
+++ resources/views/api/wysiwyg.blade.php
@@ -0,0 +1,16 @@
+<script src="/libs/trumbowyg/dist/trumbowyg.min.js" type="text/javascript"></script>
+
+@if($siteLocale != 'en')
+    <script src="/libs/trumbowyg/dist/langs/{{ $siteLocale }}.min.js" type="text/javascript"></script>
+@endif
+
+@if($assembly == 'full')
+    <script>imageUploadUrl = "{{ $routeURL }}";</script>
+    <script src="/libs/trumbowyg/dist/plugins/upload/trumbowyg.upload.js" type="text/javascript"></script>
+    <script src="/libs/trumbowyg/dist/plugins/colors/trumbowyg.colors.min.js" type="text/javascript"></script>
+@endif
+
+<script src="/js/trumbowyg/attachHTMLEditor.js" type="text/javascript"></script>
+<script>attachHTMLEditorColorImageUpload("{{ $elementID }}", "{{ $siteLocale }}");</script>
+
+
