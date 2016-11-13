4586fea74020129480efa89803bcb4a1a2efd960
Anonim_Khb <alvo4444@gmail.com>
2016-11-13 17:57:17 +1000

    Change parent branch

--- resources/views/admin/basis/js.blade.php
+++ resources/views/admin/basis/js.blade.php
@@ -1,6 +1,7 @@
-<script src='/libs/jquery/jquery_223.js'></script>
-<script src='/libs/bootstrap/js/bootstrap.js'></script>
+<script src='/libs/jquery/jquery.min.js'></script>
+<script src='/libs/bootstrap/js/bootstrap.min.js'></script>
 <script src="/libs/adminLTE/dist/js/app.min.js"></script>
 <script src="/libs/adminLTE/plugins/fastclick/fastclick.min.js"></script>
 <script src="/libs/adminLTE/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="/libs/tinymce/tinymce.min.js"></script>
 <script src="/js/main.js"></script>
