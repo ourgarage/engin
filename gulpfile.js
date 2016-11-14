e6cb2e7f58b742aea2937dd631c2b37f91bd8332
Anonim_Khb <alvo4444@gmail.com>
2016-11-14 20:33:39 +1000

    Remove folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const elixir = require('laravel-elixir');
 
 elixir(function(mix) {
-    mix.copy('node_modules/jquery/dist', 'public/libs/downloaded/jquery');
-    mix.copy('node_modules/bootstrap/dist', 'public/libs/downloaded/bootstrap');
-    mix.copy('node_modules/font-awesome/css', 'public/libs/downloaded/font-awesome/css');
-    mix.copy('node_modules/font-awesome/fonts', 'public/libs/downloaded/font-awesome/fonts');
-    mix.copy('node_modules/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css', 'public/libs/downloaded/awesome-bootstrap-checkbox');
-    mix.copy('node_modules/tinymce', 'public/libs/downloaded/tinymce');
-    mix.copy('node_modules/vue/dist', 'public/libs/downloaded/vue');
-    mix.copy('node_modules/vue-resource/dist', 'public/libs/downloaded/vue-resource');
-    mix.copy('node_modules/eonasdan-bootstrap-datetimepicker/build', 'public/libs/downloaded/eonasdan-bootstrap-datetimepicker');
-    mix.copy('node_modules/spinline/src', 'public/libs/downloaded/spinline');
+    mix.copy('node_modules/jquery/dist', 'public/libs/jquery');
+    mix.copy('node_modules/bootstrap/dist', 'public/libs/bootstrap');
+    mix.copy('node_modules/font-awesome/css', 'public/libs/font-awesome/css');
+    mix.copy('node_modules/font-awesome/fonts', 'public/libs/font-awesome/fonts');
+    mix.copy('node_modules/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css', 'public/libs/awesome-bootstrap-checkbox');
+    mix.copy('node_modules/tinymce', 'public/libs/tinymce');
+    mix.copy('node_modules/vue/dist', 'public/libs/vue');
+    mix.copy('node_modules/vue-resource/dist', 'public/libs/vue-resource');
+    mix.copy('node_modules/eonasdan-bootstrap-datetimepicker/build', 'public/libs/eonasdan-bootstrap-datetimepicker');
+    mix.copy('node_modules/spinline/src', 'public/libs/spinline');
+    mix.copy('node_modules/admin-lte/dist', 'public/libs/adminLTE/dist');
+    mix.copy('node_modules/admin-lte/plugins', 'public/libs/adminLTE/plugins');
 });
