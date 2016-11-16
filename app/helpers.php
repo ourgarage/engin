09ac967c04bf89fd9a5fd0b4d014d39da67ee49b
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-11-17 00:03:10 +1100

    fixed

--- app/helpers.php
+++ app/helpers.php
@@ -4,29 +4,27 @@ use Carbon\Carbon;
 use App\Constant\Dates;
 
 /**
- * @param date $date
+ * @param string $date
  * @param string $format
  * @return date
  */
 function df($date, $format = Dates::FORMAT_LONG)
 {
+    $date = Carbon::parse($date);
+    if ($format === Dates::TYPE_AGO) {
+        Carbon::setLocale(app()->getLocale());
+
+        return $date->diffForHumans();
+    }
+
     $mapping = [
         Dates::FORMAT_LONG => 'date.format.long.php',
         Dates::FORMAT_SHORT => 'date.format.short.php',
         Dates::FORMAT_FULL => 'date.format.full.php',
         Dates::FORMAT_TIME => 'date.format.time.php',
-        Dates::TYPE_AGO => 'ago',
     ];
 
     $key = isset($mapping[$format]) ? $mapping[$format] : $mapping[Dates::FORMAT_LONG];
 
-    if ($key === Dates::TYPE_AGO) {
-        Carbon::setLocale(app()->getLocale());
-
-        return Carbon::parse($date)->diffForHumans();
-    }
-
-    return Carbon::parse($date)->format(trans($key));
-
-
+    return $date->format(trans($key));
 }
