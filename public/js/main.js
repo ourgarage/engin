787e8a72047327223e682081bf08a7d14b331d2e
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-12-22 10:17:51 +1100

    Event complete

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
+$(function () {
+    $("[data-confirm]").on("click", function () {
+        var confirm = $(this).data("confirm");
+        buttonConfirmation(event, confirm);
+    });
+});
+
 function buttonConfirmation(e, text) {
     if (confirm(text)) {
         return true;
